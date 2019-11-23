f95692eddbcb8d4f9d2b4ba065033b517df180ed
Varun Dey <varundey20@gmail.com>
2019-11-23 15:00:48 +0530

    Create Board with user input size

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,27 @@
 import React, {Component} from "react";
 
 export default class Board extends Component {
+	constructor(props) {
+		super(props);
+		const {gridSize} = this.props;
+		this.state = {
+			playerOneTurn: true,
+			grids: Array(gridSize).fill(1).map(_ => Array(gridSize).fill(2))
+		}
+	}
+
 	render() {
 		const {gridSize} = this.props;
+		const {grids} = this.state;
 		return(
 				<div>
-					{gridSize}
+					{grids.map((row, rowIndex) => (
+							row.map((column, columnIndex) => (
+									<div key={columnIndex}>
+										{column}
+									</div>
+							))
+					))}
 				</div>
 		)
 	}
