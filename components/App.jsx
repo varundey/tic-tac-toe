8421649d2048fd9144f50aecaa812ba49eed2a93
Varun Dey <varundey20@gmail.com>
2020-05-30 22:50:17 +0530

    Move files/dir to root

---
+++ components/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "../src/styles/App.css";
+import Board from "./Board";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridSize: 3,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <label>
+          Enter the grid size:
+          <input
+            type="number"
+            min={3}
+            defaultValue={3}
+            onChange={(event) =>
+              this.setState({ gridSize: Number(event.target.value) })
+            }
+          />
+        </label>
+        <Board gridSize={this.state.gridSize} key={this.state.gridSize} />
+      </div>
+    );
+  }
+}
